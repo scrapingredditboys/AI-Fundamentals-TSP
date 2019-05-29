dfafd8e0d9adb87997886ef9a835be5f49b02871
Bartłomiej Szymański <szymbar1513@gmail.com>
2019-05-29 15:13:49 +0200

    Scripts to generate charts updated

--- plots/AntColonyPerformance.R
+++ plots/AntColonyPerformance.R
@@ -29,6 +29,40 @@ xql662ant13 <- read_csv("xql662_results_ants-13_iters-50_alpha-0.5_beta-12.0_q0-
 xql662ant15 <- read_csv("xql662_results_ants-15_iters-50_alpha-0.5_beta-12.0_q0-0.3_rho-0.6.csv", 
                         col_names = FALSE)
 
+pka379beta2 <- read_csv("results_ants-4_iters-50_alpha-0.5_beta-2.0_q0-0.3_rho-0.6.csv", 
+                       col_names = FALSE)
+pka379beta7 <- read_csv("results_ants-4_iters-50_alpha-0.5_beta-7.0_q0-0.3_rho-0.6.csv", 
+                       col_names = FALSE)
+pka379beta12 <- read_csv("results_ants-4_iters-50_alpha-0.5_beta-12.0_q0-0.3_rho-0.6.csv", 
+                       col_names = FALSE)
+pka379beta17 <- read_csv("results_ants-4_iters-50_alpha-0.5_beta-17.0_q0-0.3_rho-0.6.csv", 
+                       col_names = FALSE)
+pka379beta22 <- read_csv("results_ants-4_iters-50_alpha-0.5_beta-22.0_q0-0.3_rho-0.6.csv", 
+                        col_names = FALSE)
+
+pka379alpha1 <- read_csv("results_ants-4_iters-50_alpha-0.1_beta-12.0_q0-0.3_rho-0.6.csv", 
+                        col_names = FALSE)
+pka379alpha3 <- read_csv("results_ants-4_iters-50_alpha-0.3_beta-12.0_q0-0.3_rho-0.6.csv", 
+                        col_names = FALSE)
+pka379alpha5 <- read_csv("results_ants-4_iters-50_alpha-0.5_beta-12.0_q0-0.3_rho-0.6.csv", 
+                         col_names = FALSE)
+pka379alpha7 <- read_csv("results_ants-4_iters-50_alpha-0.7_beta-12.0_q0-0.3_rho-0.6.csv", 
+                         col_names = FALSE)
+pka379alpha9 <- read_csv("results_ants-4_iters-50_alpha-0.9_beta-12.0_q0-0.3_rho-0.6.csv", 
+                         col_names = FALSE)
+
+pka379custom1 <- read_csv("results_ants-4_iters-50_alpha-0.5_beta-12.0_q0-0.1_rho-0.99.csv", 
+                         col_names = FALSE)
+pka379custom2 <- read_csv("results_ants-4_iters-50_alpha-0.3_beta-12.0_q0-0.3_rho-0.6.csv", 
+                         col_names = FALSE)
+pka379custom3 <- read_csv("results_ants-4_iters-50_alpha-0.5_beta-12.0_q0-0.5_rho-0.5.csv", 
+                         col_names = FALSE)
+pka379custom4 <- read_csv("results_ants-4_iters-50_alpha-0.5_beta-12.0_q0-0.6_rho-0.3.csv", 
+                         col_names = FALSE)
+pka379custom5 <- read_csv("results_ants-4_iters-50_alpha-0.5_beta-12.0_q0-0.99_rho-0.1.csv", 
+                         col_names = FALSE)
+
+
 colors <- c("blue", "green", "red", "yellow", "grey", "magenta3")
 
 plottingFunction<-function(nameOfTheFile) {
@@ -62,5 +96,94 @@ plottingFunction<-function(nameOfTheFile) {
   legend("topright", legend=legendLabels, col=c("orange", colors), lwd=3)
 }
 
+plottingFunctionBeta<-function(nameOfTheFile) {
+  upperBound <- c()
+  lowerBound <- c()
+  for (i in 2:22){
+    if (i %% 5 != 2){
+      next
+    }
+    upperBound <- c(upperBound, head(get(paste(nameOfTheFile,"beta", i, sep=""))$X3, n=1))
+    lowerBound <- c(lowerBound, tail(get(paste(nameOfTheFile,"beta", i, sep=""))$X3, n=1))
+  }
+  
+  plot(get(paste(nameOfTheFile,"beta2", sep=""))$X1,
+       get(paste(nameOfTheFile,"beta2", sep=""))$X3, 
+       ylim = c(min(lowerBound), max(upperBound)+200),
+       main=paste("Ant Colony Algorithm depending on beta coefficient (calculated in the first 50 iterations on", 
+                  nameOfTheFile, "file)"), ylab="Calculated distance", 
+       xlab="Iterations per ant", type="l", col="orange", lty=1:2, lwd=3, log="y")
+  
+  legendLabels <- c("Beta parameter = 2")
+  for (i in 7:22){
+    if (i %% 5 !=2){
+      next
+    }
+    lines(get(paste(nameOfTheFile,"beta", i, sep=""))$X1,
+          get(paste(nameOfTheFile,"beta", i, sep=""))$X3, 
+          type="l", col=colors[(i-2)/5], cex=0.8, lwd=3)
+    legendLabels <- c(legendLabels, paste("Beta parameter =", i))
+  }
+  legend("topright", legend=legendLabels, col=c("orange", colors), lwd=3)
+}
+
+plottingFunctionAlpha<-function(nameOfTheFile) {
+  upperBound <- c()
+  lowerBound <- c()
+  for (i in 1:9){
+    if (i %% 2 != 1){
+      next
+    }
+    upperBound <- c(upperBound, head(get(paste(nameOfTheFile,"alpha", i, sep=""))$X3, n=1))
+    lowerBound <- c(lowerBound, tail(get(paste(nameOfTheFile,"alpha", i, sep=""))$X3, n=1))
+  }
+  
+  plot(get(paste(nameOfTheFile,"alpha1", sep=""))$X1,
+       get(paste(nameOfTheFile,"alpha1", sep=""))$X3, 
+       ylim = c(min(lowerBound), max(upperBound)),
+       main=paste("Ant Colony Algorithm depending on alpha coefficient (calculated in the first 50 iterations on", 
+                  nameOfTheFile, "file)"), ylab="Calculated distance", 
+       xlab="Iterations per ant", type="l", col="orange", lty=1:2, lwd=3, log="y")
+  
+  legendLabels <- c("Alpha parameter = .1")
+  for (i in 3:9){
+    if (i %% 2 != 1){
+      next
+    }
+    lines(get(paste(nameOfTheFile,"alpha", i, sep=""))$X1,
+          get(paste(nameOfTheFile,"alpha", i, sep=""))$X3, 
+          type="l", col=colors[(i-1)/2], cex=0.8, lwd=3)
+    legendLabels <- c(legendLabels, paste("Alpha parameter = .", i, sep=""))
+  }
+  legend("topright", legend=legendLabels, col=c("orange", colors), lwd=3)
+}
+
+plottingFunctionCustom<-function(nameOfTheFile) {
+  upperBound <- c()
+  lowerBound <- c()
+  for (i in 1:5){
+    upperBound <- c(upperBound, head(get(paste(nameOfTheFile,"custom", i, sep=""))$X3, n=1))
+    lowerBound <- c(lowerBound, tail(get(paste(nameOfTheFile,"custom", i, sep=""))$X3, n=1))
+  }
+  
+  plot(get(paste(nameOfTheFile,"custom1", sep=""))$X1,
+       get(paste(nameOfTheFile,"custom1", sep=""))$X3, 
+       ylim = c(min(lowerBound), max(upperBound)),
+       main=paste("Ant Colony Algorithm depending on other coefficients (calculated in the first 50 iterations on", 
+                  nameOfTheFile, "file)"), ylab="Calculated distance", 
+       xlab="Iterations per ant", type="l", col="orange", lty=1:2, lwd=3, log="y")
+  
+  legendLabels <- c("q0=0.1, rho=0.99", "q0=0.3, rho=0.6", "q0=0.5, rho=0.5", "q0=0.6, rho=0.3", "q0=0.99, rho=0.1")
+  for (i in 2:5){
+    lines(get(paste(nameOfTheFile,"custom", i, sep=""))$X1,
+          get(paste(nameOfTheFile,"custom", i, sep=""))$X3, 
+          type="l", col=colors[(i-1)], cex=0.8, lwd=3)
+  }
+  legend("topright", legend=legendLabels, col=c("orange", colors), lwd=3)
+}
+
 plottingFunction("pka379")
 plottingFunction("xql662")
+plottingFunctionBeta("pka379")
+plottingFunctionAlpha("pka379")
+plottingFunctionCustom("pka379")
--- plots/SimulatedAnnealingPerformance.R
+++ plots/SimulatedAnnealingPerformance.R
@@ -1,49 +1,139 @@
 library(readr)
-pka379ant3 <- read_csv("results_ants-3_iters-50_alpha-0.5_beta-12.0_q0-0.3_rho-0.6.csv", 
-                       col_names = FALSE)
-pka379ant5 <- read_csv("results_ants-5_iters-50_alpha-0.5_beta-12.0_q0-0.3_rho-0.6.csv", 
-                       col_names = FALSE)
-pka379ant7 <- read_csv("results_ants-7_iters-50_alpha-0.5_beta-12.0_q0-0.3_rho-0.6.csv", 
-                       col_names = FALSE)
-pka379ant9 <- read_csv("results_ants-9_iters-50_alpha-0.5_beta-12.0_q0-0.3_rho-0.6.csv", 
-                       col_names = FALSE)
-pka379ant11 <- read_csv("results_ants-11_iters-50_alpha-0.5_beta-12.0_q0-0.3_rho-0.6.csv", 
-                        col_names = FALSE)
-pka379ant13 <- read_csv("results_ants-13_iters-50_alpha-0.5_beta-12.0_q0-0.3_rho-0.6.csv", 
-                        col_names = FALSE)
-pka379ant15 <- read_csv("results_ants-15_iters-50_alpha-0.5_beta-12.0_q0-0.3_rho-0.6.csv", 
-                        col_names = FALSE)
-colors()
-colors <- c("blue", "green", "red", "yellow", "grey", "magenta3")
+pka379DataBestSettings <- read_csv("pka379DataBestSettings.csv")
+pka379DataSwapPoints <- read_csv("pka379DataSwapPoints.csv")
+pka379DataInsertingPoints <- read_csv("pka379DataInsertingPoints.csv")
+pka379DataReverseSubpath <- read_csv("pka379DataReverseSubpath.csv")
+xql662DataBestSettings <- read_csv("xql662DataBestSettings.csv")
+xql662DataSwapPoints <- read_csv("xql662DataSwapPoints.csv")
+xql662DataInsertingPoints <- read_csv("xql662DataInsertingPoints.csv")
+xql662DataReverseSubpath <- read_csv("xql662DataReverseSubpath.csv")
+
+pka379Data250iter <- read_csv("pka379Data250iter.csv")
+pka379Data500iter <- read_csv("pka379Data500iter.csv")
+pka379Data750iter <- read_csv("pka379Data750iter.csv")
+pka379Data1000iter <- read_csv("pka379Data1000iter.csv")
+pka379Data1250iter <- read_csv("pka379Data1250iter.csv")
+pka379Data1500iter <- read_csv("pka379Data1500iter.csv")
+
+pka379DataPoint941CoolingRate <- read_csv("pka379Data.941CoolingRate.csv")
+pka379DataPoint96CoolingRate <- read_csv("pka379Data.96CoolingRate.csv")
+pka379DataPoint98CoolingRate <- read_csv("pka379Data.98CoolingRate.csv")
+pka379DataPoint99CoolingRate <- read_csv("pka379Data.99CoolingRate.csv")
+pka379DataPoint995CoolingRate <- read_csv("pka379Data.995CoolingRate.csv")
 
 plottingFunction<-function(nameOfTheFile) {
+  plot(get(paste(nameOfTheFile,"DataReverseSubpath", sep=""))$X1,
+       get(paste(nameOfTheFile,"DataReverseSubpath", sep=""))$Distance, 
+       xaxt='n', main=paste("Simulated Annealing performance (between temperatures 100 and 0.2 on", 
+                            nameOfTheFile, "file from Waterloo University website)"), ylab="Calculated distance", 
+       xlab="Temperature", type="l", col="orange", lty=1:2, lwd=3)
+  axis(1, at=c(0, 114, 227, 308), labels=c(98, 10, 1, 0.2));
+  lines(get(paste(nameOfTheFile,"DataSwapPoints", sep=""))$X1,
+        get(paste(nameOfTheFile,"DataSwapPoints", sep=""))$Distance, 
+        type="l", col="blue", cex=0.8, lwd=3)
+  lines(get(paste(nameOfTheFile,"DataInsertingPoints", sep=""))$X1,
+        get(paste(nameOfTheFile,"DataInsertingPoints", sep=""))$Distance, 
+        type="l", col="green", cex=0.8, lwd=3)
+  lines(get(paste(nameOfTheFile,"DataBestSettings", sep=""))$X1,
+        get(paste(nameOfTheFile,"DataBestSettings", sep=""))$Distance, 
+        type="l", col="red", cex=0.8, lwd=3)
+  legend("topright", legend=c("Combined three methods", "Only Swap Points enabled", "Only Inserting Points enabled", "Only Reverse Subpath enabled"),
+         col=c("red", "blue", "green", "orange"), lwd=3)
+}
+upperBound <- c()
+lowerBound <- c()
+
+plottingIterFunction<-function(nameOfTheFile) {
   upperBound <- c()
   lowerBound <- c()
-  for (i in 3:15){
-    if (!i %% 2){
+  colors <- c("blue", "green", "red", "yellow", "grey")
+  for (i in 250:1500){
+    if (i %% 250 != 0){
       next
     }
-    upperBound <- c(upperBound, head(paste(nameOfTheFile,"ant", i, sep="")[["X3"]], n=1))
-    lowerBound <- c(lowerBound, tail(paste(nameOfTheFile,"ant", i, sep="")[["X3"]], n=1))
+    upperBound <- c(upperBound, head(get(paste(nameOfTheFile,"Data", i, "iter", sep=""))$Distance, n=1))
+    lowerBound <- c(lowerBound, tail(get(paste(nameOfTheFile,"Data", i, "iter", sep=""))$Distance, n=1))
   }
-  
-  plot(get(paste(nameOfTheFile,"ant3", sep=""))$X1,
-       get(paste(nameOfTheFile,"ant3", sep=""))$X3, 
-       ylim = c(min(lowerBound), max(upperBound)),
-       main=paste("Ant Colony Algorithm performance (calculated in the first 50 iterations on", 
-       nameOfTheFile, "file from Waterloo University website)"), ylab="Calculated distance", 
-       xlab="Iterations per ant", type="l", col="orange", lty=1:2, lwd=3)
-  legendLabels <- c("Ant Colony algorithm with 3 ants")
-  for (i in 5:15){
-    if (!i %% 2){
+  plot(get(paste(nameOfTheFile,"Data250iter", sep=""))$X1,
+       get(paste(nameOfTheFile,"Data250iter", sep=""))$Distance, 
+       xaxt='n', main=paste("Simulated Annealing performance (dependent on the amount of iterations on",
+                            nameOfTheFile, "file)"), ylim = c(min(lowerBound), max(upperBound)),
+                            ylab="Calculated distance", 
+                            xlab="Temperature", type="l", col="orange", lty=1:2, lwd=3)
+  axis(1, at=c(0, 114, 227, 308), labels=c(98, 10, 1, 0.2));
+  legendLabels <- c("250 iterations")
+  for (i in 500:1500){
+    if (i %% 250 != 0){
       next
     }
-    lines(get(paste(nameOfTheFile,"ant", i, sep=""))$X1,
-          get(paste(nameOfTheFile,"ant", i, sep=""))$X3, 
-          type="l", col=colors[(i-5)/2], cex=0.8, lwd=3)
-    legendLabels <- c(legendLabels, paste("Ant Colony algorithm with", i, "ants"))
+    lines(get(paste(nameOfTheFile,"Data", i, "iter", sep=""))$X1,
+          get(paste(nameOfTheFile,"Data", i, "iter", sep=""))$Distance, 
+          type="l", col=colors[(i-250)/250], cex=0.8, lwd=3)
+    legendLabels <- c(legendLabels, paste(i, "iterations"))
   }
   legend("topright", legend=legendLabels, col=c("orange", colors), lwd=3)
 }
 
+cleaningUpFiles <- function() {
+  pka379DataPoint99CoolingRateEdited <- pka379DataPoint99CoolingRate[seq(1, nrow(pka379DataPoint99CoolingRate), 2),]
+  pka379DataPoint99CoolingRateEdited$X1 <- 0:309
+  pka379DataPoint99CoolingRateEdited <- head(pka379DataPoint99CoolingRateEdited, -2) 
+  
+  pka379DataPoint995CoolingRateEdited <- pka379DataPoint995CoolingRate[seq(1, nrow(pka379DataPoint995CoolingRate), 4),]
+  pka379DataPoint995CoolingRateEdited$X1 <- 0:309
+  pka379DataPoint995CoolingRateEdited <- head(pka379DataPoint995CoolingRateEdited, -2) 
+  
+  pka379DataPoint96CoolingRateEdited <- pka379DataPoint96CoolingRate[rep(seq_len(nrow(pka379DataPoint96CoolingRate)), each=2),]
+  temp <- data.frame(pka379DataPoint96CoolingRateEdited$X1[306], pka379DataPoint96CoolingRateEdited$Temperature[306], pka379DataPoint96CoolingRateEdited$Distance[306])
+  colnames(temp) <- c("X1", "Temperature", "Distance")
+  pka379DataPoint96CoolingRateEdited <- rbind(pka379DataPoint96CoolingRateEdited, temp)
+  pka379DataPoint96CoolingRateEdited <- rbind(pka379DataPoint96CoolingRateEdited, temp)
+  pka379DataPoint96CoolingRateEdited$X1 <- 0:307
+  
+  pka379DataPoint941CoolingRateEdited <- pka379DataPoint941CoolingRate[rep(seq_len(nrow(pka379DataPoint941CoolingRate)), each=3),]
+  pka379DataPoint941CoolingRateEdited <- head(pka379DataPoint941CoolingRateEdited, -1) 
+  pka379DataPoint941CoolingRateEdited$X1 <- 0:307
+}
+
+
+plottingCoolingFunction<-function(nameOfTheFile) {
+  upperBound <- c(head(get(paste(nameOfTheFile,"DataPoint941CoolingRateEdited", sep=""))$Distance),
+                  head(get(paste(nameOfTheFile,"DataPoint96CoolingRateEdited", sep=""))$Distance),
+                  head(get(paste(nameOfTheFile,"DataPoint98CoolingRate", sep=""))$Distance),
+                  head(get(paste(nameOfTheFile,"DataPoint99CoolingRateEdited", sep=""))$Distance),
+                  head(get(paste(nameOfTheFile,"DataPoint995CoolingRateEdited", sep=""))$Distance))
+  lowerBound <- c(tail(get(paste(nameOfTheFile,"DataPoint941CoolingRateEdited", sep=""))$Distance),
+                  tail(get(paste(nameOfTheFile,"DataPoint96CoolingRateEdited", sep=""))$Distance),
+                  tail(get(paste(nameOfTheFile,"DataPoint98CoolingRate", sep=""))$Distance),
+                  tail(get(paste(nameOfTheFile,"DataPoint99CoolingRateEdited", sep=""))$Distance),
+                  tail(get(paste(nameOfTheFile,"DataPoint995CoolingRateEdited", sep=""))$Distance))
+  colors <- c("blue", "green", "orange", "red", "yellow")
+  plot(get(paste(nameOfTheFile,"DataPoint98CoolingRate", sep=""))$X1,
+       get(paste(nameOfTheFile,"DataPoint98CoolingRate", sep=""))$Distance, 
+       xaxt='n', main=paste("Simulated Annealing performance (dependent on the amount of the cooling rate on",
+                            nameOfTheFile, "file)"), ylim = c(min(lowerBound), max(upperBound)),
+       ylab="Calculated distance", 
+       xlab="Temperature", type="l", col=colors[3], lty=1:2, lwd=3)
+  axis(1, at=c(0, 114, 227, 308), labels=c(100, 10, 1, 0.2));
+  
+  legendLabels <- c(".941 Cooling Rate", ".96 Cooling Rate", ".98 Cooling Rate", ".99 Cooling Rate", ".995 Cooling Rate")
+  lines(get(paste(nameOfTheFile, "DataPoint941CoolingRateEdited", sep=""))$X1,
+        get(paste(nameOfTheFile, "DataPoint941CoolingRateEdited", sep=""))$Distance, 
+        type="l", col=colors[1], cex=0.8, lwd=3)
+  lines(get(paste(nameOfTheFile, "DataPoint96CoolingRateEdited", sep=""))$X1,
+        get(paste(nameOfTheFile, "DataPoint96CoolingRateEdited", sep=""))$Distance, 
+        type="l", col=colors[2], cex=0.8, lwd=3)
+  lines(get(paste(nameOfTheFile, "DataPoint99CoolingRateEdited", sep=""))$X1,
+        get(paste(nameOfTheFile, "DataPoint99CoolingRateEdited", sep=""))$Distance, 
+        type="l", col=colors[4], cex=0.8, lwd=3)
+  lines(get(paste(nameOfTheFile, "DataPoint995CoolingRateEdited", sep=""))$X1,
+        get(paste(nameOfTheFile, "DataPoint995CoolingRateEdited", sep=""))$Distance, 
+        type="l", col=colors[5], cex=0.8, lwd=3)
+  legend("topright", legend=legendLabels, col=c(colors), lwd=3)
+}
+
 plottingFunction("pka379")
+plottingFunction("xql662")
+plottingIterFunction("pka379")
+cleaningUpFiles()
+plottingCoolingFunction("pka379")
